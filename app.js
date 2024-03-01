876f0f91465fd85924a8715769dfded8f4c1e1ae
Syedanaskhi51 <154814876+Syedanaskhi51@users.noreply.github.com>
2024-03-02 01:54:07 +0500

    javascript file create

---
+++ app.js
@@ -0,0 +1,130 @@
+
+//Familiar Operators
+
+//Addition
+
+var num1 = 30
+var num2 = 20
+var addition = num1 + num2 
+console.log( addition) // answer(50)
+
+var num3 = 50
+var num4 = 30
+var addition = num3 + num4
+console.log( addition) // answer(80)
+
+
+//Subtraction
+
+var num5 = 40
+var num6 = 10
+var Subtraction = num5 - num6
+console.log( Subtraction) // answer(30)
+
+
+
+var num7 = 100
+var num8 = 40
+var subtraction = num7 - num8
+console.log(subtraction) // answer(25)
+
+
+
+//Multiplication 
+
+
+var num9 = 5
+var num10 = 5
+var multiplication = num9 * num10
+console.log ( multiplication)// answer(25)
+
+var num11 = 30
+var num12 = 4
+var multiplication = num11 * num12
+console.log( multiplication)// answer(120)
+
+
+//Divide
+
+var num13 = 30
+var num14 = 10
+var divide = num13 / num14 
+console.log(divide)// answer(3)
+
+
+var num15 = 20
+var num16 = 10
+var modulas = num15 / num16
+console.log(divide)// answer(3)
+
+
+
+//Modulas
+
+var num17 = 5 
+var num18 = 3
+var modulas = 5 % 3
+console.log( modulas)// answer(2)
+
+
+var num19 = 12
+var num20 = 9
+var modulas = num19 % num20
+console.log(modulas)// answer(3)
+
+
+
+
+
+//Exponentiation
+
+var num21 = 5
+var num22 = 3
+var Exponentiation = num21 ** num22 // 5 * 5 * 5 
+console.log( Exponentiation)// answer(125)
+
+
+
+//B O D M A S
+  
+var bodmas1 = multiplication / Subtraction + addition
+console.log( bodmas1 )
+
+
+var bodmas2 = 20 / Subtraction + 100
+console.log(bodmas2)
+
+var bodmas3 = multiplication + subtraction * modulas
+console.log(bodmas3)
+
+
+
+// Unfamiliar 
+
+//Increment ++
+//Dicrement--
+
+ 
+
+//Pre Inrement / Decrement  ( ++num , --num )
+
+var num00 = 10
+var num01 = ++num00
+console.log(num00)
+
+var num09 = 20
+var num02 = --num09
+console.log(num09)
+
+
+//post Increment / Decrement  ( num++ , num-- )
+
+ var num99 = 30
+ var num03 = num99++
+ console.log(num99)
+ console.log(num03)
+
+ var num88 = 30
+ var num04 = num88--
+ console.log(num88)
+ console.log(num04)
